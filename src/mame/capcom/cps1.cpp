93b0dac8f94af655b1a93367b0bb930cfbaab9e7
Ryan Souders <honkeykong@honkeykong.org>
2026-03-26 00:43:06 -0500

    capcom/cps1: log sf2hf timing init fingerprint

--- src/mame/capcom/cps1.cpp
+++ src/mame/capcom/cps1.cpp
@@ -14880,6 +14880,7 @@ void cps_state::init_sf2hf_timing()
 	m_sf2hf_vblank_frame = 0;
 	m_sf2hf_sample_frames = 0;
 	m_maincpu->set_clock_scale(SF2HF_TIMING_CLOCK_SCALE);
+	machine().logerror("sf2hf timing init scale=%.9f avg_log_frames=300\n", SF2HF_TIMING_CLOCK_SCALE);
 }
 
 uint16_t cps_state::ganbare_ram_r(offs_t offset, uint16_t mem_mask)
